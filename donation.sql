11cb7fd87a50c7f3ab01bdb027648680494cf80b
sanyukta16 <95396981+sanyukta16@users.noreply.github.com>
2022-02-28 15:09:18 +0530

    Add files via upload

---
+++ donation.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 11, 2022 at 08:51 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `donation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation_details`
+--
+
+CREATE TABLE `donation_details` (
+  `name` varchar(255) NOT NULL,
+  `phone_number` int(10) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `amount` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `donation_details`
+--
+
+INSERT INTO `donation_details` (`name`, `phone_number`, `email`, `amount`) VALUES
+('Sanyukta Borikar', 8308596468, 'sanyuktaborikar16@gmail.com', 5000),
+;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
